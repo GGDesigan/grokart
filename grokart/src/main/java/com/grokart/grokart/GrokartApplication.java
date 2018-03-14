cbb9b27933b49a2f1f62e12670b705034dfd8418
Desigan <desigan@192.168.1.72>
2018-03-14 15:32:22 +0530

    Initial Project

---
+++ grokart/src/main/java/com/grokart/grokart/GrokartApplication.java
@@ -0,0 +1,12 @@
+package com.grokart.grokart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrokartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrokartApplication.class, args);
+	}
+}
---
+++ grokart/src/test/java/com/grokart/grokart/GrokartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grokart.grokart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrokartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
